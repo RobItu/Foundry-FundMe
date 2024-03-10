859544e95ac7760c9156603d9fda7e5833a6a66b
robitu <robertoiturralde2009@hotmail.com>
2024-03-10 17:54:11 -0600

    First Commit

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    // SetUp always runs first!
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMinimumDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function test_isOwner() public {
+        assertEq(fundMe.i_owner(), address(this));
+    }
+}
